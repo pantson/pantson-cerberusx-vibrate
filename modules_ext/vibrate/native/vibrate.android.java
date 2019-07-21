6149ff0e301de7005b41c0349326fd6461a78ef7
pantson <35967917+pantson@users.noreply.github.com>
2019-07-21 23:22:01 +0100

    Initial commit

---
+++ modules_ext/vibrate/native/vibrate.android.java
@@ -0,0 +1,33 @@
+import android.os.Vibrator;
+import android.os.VibrationEffect;
+
+class Vibrate {
+	// info from https://stackoverflow.com/questions/4870667/how-can-i-use-getsystemservice-in-a-non-activity-class-locationmanager
+  // effect info https://developer.android.com/reference/android/os/VibrationEffect to be fixed in API29 wrapper
+	Activity activity = BBAndroidGame._androidGame._activity;
+  Vibrator v = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
+  
+	public void vibrate(int mills,int effect){
+     switch (effect) {
+       case 0:
+       effect = VibrationEffect.DEFAULT_AMPLITUDE;
+/*
+       case 1:
+       effect = VibrationEffect.EFFECT_CLICK;
+       case 2:
+       effect = VibrationEffect.EFFECT_DOUBLE_CLICK;
+       case 3:
+       effect = VibrationEffect.EFFECT_HEAVY_CLICK;
+       case 4:
+       effect = VibrationEffect.EFFECT_TICK;
+*/
+     }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      v.vibrate(VibrationEffect.createOneShot(mills, effect));
+    } else {
+      //deprecated in API 26 
+      v.vibrate(500);
+    }
+	}
+
+}
