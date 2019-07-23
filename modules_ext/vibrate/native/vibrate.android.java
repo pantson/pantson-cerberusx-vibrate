e1374ce63eb2d89248e954101f16d711ad172cf9
pantson <35967917+pantson@users.noreply.github.com>
2019-07-23 21:56:06 +0100

    Update vibrate.android.java
    fixed millisecs not being used in older APIs (was set to 500 millisecs)
    fixed spacing

--- modules_ext/vibrate/native/vibrate.android.java
+++ modules_ext/vibrate/native/vibrate.android.java
@@ -2,32 +2,31 @@ import android.os.Vibrator;
 import android.os.VibrationEffect;
 
 class Vibrate {
-	// info from https://stackoverflow.com/questions/4870667/how-can-i-use-getsystemservice-in-a-non-activity-class-locationmanager
+  // info from https://stackoverflow.com/questions/4870667/how-can-i-use-getsystemservice-in-a-non-activity-class-locationmanager
   // effect info https://developer.android.com/reference/android/os/VibrationEffect to be fixed in API29 wrapper
-	Activity activity = BBAndroidGame._androidGame._activity;
+  Activity activity = BBAndroidGame._androidGame._activity;
   Vibrator v = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
   
-	public void vibrate(int mills,int effect){
+  public void vibrate(int millisecs,int effect){
      switch (effect) {
-       case 0:
-       effect = VibrationEffect.DEFAULT_AMPLITUDE;
-/*
-       case 1:
-       effect = VibrationEffect.EFFECT_CLICK;
-       case 2:
-       effect = VibrationEffect.EFFECT_DOUBLE_CLICK;
-       case 3:
-       effect = VibrationEffect.EFFECT_HEAVY_CLICK;
-       case 4:
-       effect = VibrationEffect.EFFECT_TICK;
-*/
+      case 0:
+      effect = VibrationEffect.DEFAULT_AMPLITUDE;
+      /*
+      case 1:
+      effect = VibrationEffect.EFFECT_CLICK;
+      case 2:
+      effect = VibrationEffect.EFFECT_DOUBLE_CLICK;
+      case 3:
+      effect = VibrationEffect.EFFECT_HEAVY_CLICK;
+      case 4:
+      effect = VibrationEffect.EFFECT_TICK;
+      */
      }
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      v.vibrate(VibrationEffect.createOneShot(mills, effect));
+      v.vibrate(VibrationEffect.createOneShot(millisecs, effect));
     } else {
       //deprecated in API 26 
-      v.vibrate(500);
+      v.vibrate(millisecs);
     }
-	}
-
+  }
 }
